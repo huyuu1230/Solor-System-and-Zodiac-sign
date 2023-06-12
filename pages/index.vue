5048221ca105845f25e3db7592ce9c365e6d28d5
nirnorOkazaki <okazaki@nirnor.jp>
2023-06-12 23:21:13 +0900

    不必要なものを削除

--- pages/index.vue
+++ pages/index.vue
@@ -30,31 +30,6 @@ onMounted(() => {
 
     camera.position.set(0, 0, +1500);
 
-    // -----Geometry___オブジェクトの形 , 大きさなど
-    const geometry = new THREE.SphereGeometry(400, 30, 30);
-    // -----Material___オブジェクトの質感 , 色など
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x000000,
-      wireframe: true,
-    });
-    // -----Mesh___Geometry と Materialをまとめて一つのオブジェクトにする
-    const box = new THREE.Mesh(geometry, material);
-
-    // -----半径
-    const radius = 800;
-    // -----仰角
-    let phi = (toRad(45) * Math.PI) / 180;
-    // -----方位角
-    let theta = ((toRad(45) - 180) * Math.PI) / 180;
-    // -----x座標
-    const x = -1 * radius * Math.cos(phi) * Math.cos(theta);
-    // -----y座標
-    const y = radius * Math.sin(phi);
-    // -----z座標
-    const z = radius * Math.cos(phi) * Math.sin(theta);
-    // -----position
-    box.position.set(x, y, z);
-    scene.add(box);
 
     const sphereGeometry = new THREE.SphereGeometry(200, 20, 20);
     const sphereMaterial = new THREE.MeshBasicMaterial({
@@ -77,22 +52,6 @@ onMounted(() => {
 
     // 毎フレーム時に実行されるループイベントです
     function tick() {
-      box.rotation.x += 0.01;
-      box.rotation.y += 0.01;
-
-      // -----加算する角度
-      phi += toRad(0.5);
-      // -----加算する方位角
-      theta += toRad(0.5);
-      // -----x座標
-      const x = -2 * radius * Math.cos(phi) * Math.cos(theta);
-      // -----y座標
-      const y = radius * Math.sin(phi);
-      // -----z座標
-      const z = -2 *radius * Math.cos(phi) * Math.sin(theta);
-      // -----position
-      box.position.set(x, y, z);
-
       // ----------SPHERE
       // -----rotate
       sphere.rotation.x -= 0.01;
