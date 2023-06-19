6315fd2c3b139d83a88e5622fd464a7ae121b078
nirnorOkazaki <okazaki@nirnor.jp>
2023-06-20 01:19:32 +0900

    Planetクラスをアップグレード

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@ onMounted(() => {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
     camera = new THREE.PerspectiveCamera(100, width / height, 5000, 0);
-    camera.position.set(0, 0, +1500);
+    camera.position.set(0, 500, +2000);
 
     // -----規準となる地球の変数を定義
     const earthRaddius = 100;
@@ -59,78 +59,42 @@ onMounted(() => {
     Earth.position.set(earthX, earthY, earthZ);
     scene.add(Earth);
 
-    //----------水星
-    const mercuryRaddius = earthRaddius * 0.38;
-    const mercuryW = mercuryRaddius / 5;
-    const mercuryH = mercuryRaddius / 5;
-    const mercuryOrbitRaddius = earthOrbitRaddius * 0.39;
-    let mercuryPhi = toRad(0);
-    let mercuryTheta = toRad(0);
-    const mercuryPhiSpeed = earthPhiSpeed / 0.24;
-    const mercuryX = mercuryOrbitRaddius * Math.cos(mercuryPhi);
-    const mercuryY = mercuryOrbitRaddius * Math.sin(mercuryTheta);
-    const mercuryZ = mercuryOrbitRaddius * Math.sin(mercuryPhi);
-    // -----水星の3Dオブジェクト
-    const MercuryGeometry = new THREE.SphereGeometry(
-      mercuryRaddius,
-      mercuryW,
-      mercuryH
-    );
-    const MercuryMaterial = new THREE.MeshBasicMaterial({
-      color: 0x000000,
-      wireframe: true,
-    });
-    const Mercury = new THREE.Mesh(MercuryGeometry, MercuryMaterial);
-    Mercury.position.set(mercuryX, mercuryY, mercuryZ);
-    scene.add(Mercury);
 
-    // ----------金星
-    // function createOrbit(r,or,phi,){
-    //   const planetRaddius = earthRaddius * r;
-    //   const planetW = planetRaddius / 5;
-    //   const planetH = planetRaddius / 5;
-    //   const planetOrbitRaddius = earthOrbitRaddius * or;
-    //   let planetPhi = toRad(0);
-    //   let planetTheta = toRad(0);
-    //   const planetPhiSpeed = earthPhiSpeed / phi;
-    //   const planetX = planetOrbitRaddius * Math.cos(planetPhi);
-    //   const planetY = planetOrbitRaddius * Math.sin(planetTheta);
-    //   const planetZ = planetOrbitRaddius * Math.sin(planetPhi);
-    //   const PlanetGeometry = new THREE.SphereGeometry(
-    //     planetRaddius,
-    //     planetW,
-    //     planetH,
-    //   );
-    //   const PlanetMaterial = new THREE.MeshBasicMaterial({
-    //     color:0x000000,
-    //     wireframe:true,
-    //   });
-    //   const Planet = new THREE.Mesh(PlanetGeometry,PlanetMaterial);
-    //   Planet.position.set(planetX,planetY,planetZ);
-    //   scene.add(Planet);
-    // }
 
+
+    // ----------惑星を生成するクラス
     class Planet {
-      constructor(radius) {
+      constructor(radius, orbitRadius, phiSpeed) {
         this.r = earthRaddius * radius;
-        this.w = this.r /5;
-        this.h = this.r /5
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-
+        this.w = this.r / 5;
+        this.h = this.r / 5
+        this.or = earthOrbitRaddius * orbitRadius;
+        this.phi = toRad(0);
+        this.theta = toRad(0);
+        this.phiSpeed = earthPhiSpeed / phiSpeed;
+        this.x = this.or * Math.cos(this.phi);
+        this.y = this.or * Math.sin(this.theta);
+        this.z = this.or * Math.sin(this.phi);
         this.geometry = new THREE.SphereGeometry(this.r, this.w, this.h);
         this.material = new THREE.MeshBasicMaterial({
           color: 0x000000,
           wireframe: true,
         });
         this.planet = new THREE.Mesh(this.geometry, this.material);
-        this.planet.position.set(this.x,this.y,this.z)
+        this.planet.position.set(this.x, this.y, this.z)
         scene.add(this.planet);
       }
+      update() {
+        this.phi += this.phiSpeed;
+        this.x = this.or * Math.cos(this.phi);
+        this.y = this.or * Math.sin(this.theta);
+        this.z = this.or * Math.sin(this.phi);
+        this.planet.position.set(this.x, this.y, this.z)
+      }
     }
-    const venus = new Planet(0.95);
-   
+    const Mercury = new Planet(0.38, 0.39, 0.24);
+    const Venus = new Planet(0.95, 0.72, 0.62);
+
 
     tick();
 
@@ -144,11 +108,8 @@ onMounted(() => {
       const earthZ = earthOrbitRaddius * Math.sin(earthPhi);
       Earth.position.set(earthX, earthY, earthZ);
       // -----水星
-      mercuryPhi += mercuryPhiSpeed;
-      const mercuryX = mercuryOrbitRaddius * Math.cos(mercuryPhi);
-      const mercuryY = mercuryOrbitRaddius * Math.sin(mercuryTheta);
-      const mercuryZ = mercuryOrbitRaddius * Math.sin(mercuryPhi);
-      Mercury.position.set(mercuryX, mercuryY, mercuryZ);
+      Mercury.update();
+      Venus.update()
 
       renderer.render(scene, camera); // レンダリング
       requestAnimationFrame(tick);
