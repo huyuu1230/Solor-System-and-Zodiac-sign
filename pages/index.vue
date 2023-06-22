7c1353c7e27bc4270b7fc52aaed393ea073ecfd9
Okazaki nirnor <okazaki@nirnor.jp>
2023-06-22 17:19:14 +0900

    class Planet に軌道を表示する orbit メソッドを追加

--- pages/index.vue
+++ pages/index.vue
@@ -26,45 +26,26 @@ onMounted(() => {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
     camera = new THREE.PerspectiveCamera(100, width / height, 5000, 0);
-    camera.position.set(0, 500, +3000);
+    camera.position.set(0, 500, +2000);
 
     // -----規準となる地球の変数を定義
     const earthRaddius = 100;
-    const earthWidth = earthRaddius / 5;
-    const earthHeight = earthRaddius / 5;
     // 地球の軌道半径
     const earthOrbitRaddius = 1200;
-    let earthPhi = toRad(0);
-    let earthTheta = toRad(0);
     // 緯度(方位角)の変化する値
     const earthPhiSpeed = toRad(0.75);
     // 経度(仰角)の変化する値
     const earthThetaSpeed = toRad(0.75);
-    // x , y , z それぞれの座標
-    const earthX = earthOrbitRaddius * Math.cos(earthPhi);
-    const earthY = earthOrbitRaddius * Math.sin(earthTheta);
-    const earthZ = earthOrbitRaddius * Math.sin(earthPhi);
+    
 
-    // 地球の3Dオブジェクトを作成
-    const EarthGeometry = new THREE.SphereGeometry(
-      earthRaddius,
-      earthWidth,
-      earthHeight
-    );
-    const EarthMaterial = new THREE.MeshBasicMaterial({
-      color: 0x000000,
-      wireframe: true,
-    });
-    const Earth = new THREE.Mesh(EarthGeometry, EarthMaterial);
-    Earth.position.set(earthX, earthY, earthZ);
-    scene.add(Earth);
+    
 
     // ----------惑星を生成するクラス
     class Planet {
       constructor(radius, orbitRadius, phiSpeed) {
         this.r = earthRaddius * radius;
-        this.w = this.r / 5;
-        this.h = this.r / 5;
+        this.w = this.r / 10;
+        this.h = this.r / 10;
         this.or = earthOrbitRaddius * orbitRadius;
         this.phi = toRad(0);
         this.theta = toRad(0);
@@ -81,6 +62,26 @@ onMounted(() => {
         this.planet.position.set(this.x, this.y, this.z);
         scene.add(this.planet);
       }
+      // 軌道を生成するメソッド
+      orbit() {
+        this.orbitPoints = [];
+        this.orbitPointNum = 360;
+        for (let i = 0; i <= this.orbitPointNum; i++) {
+          const rad = toRad((360 / this.orbitPointNum) * i);
+          const x = this.or * Math.cos(rad);
+          const y = 0;
+          const z = this.or * Math.sin(rad);
+          const p = new THREE.Vector3(x, y, z);
+          this.orbitPoints.push(p);
+        }
+        this.orbitGeometry = new THREE.BufferGeometry().setFromPoints(
+          this.orbitPoints
+        );
+        this.orbitMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+        this.orbitMesh = new THREE.Line(this.orbitGeometry, this.orbitMaterial);
+        scene.add(this.orbitMesh);
+      }
+      // 惑星をアニメーションさせるメソッド
       update() {
         this.phi += this.phiSpeed;
         this.x = this.or * Math.cos(this.phi);
@@ -90,63 +91,30 @@ onMounted(() => {
       }
     }
     const Mercury = new Planet(0.38, 0.39, 0.24);
+    Mercury.orbit();
     const Venus = new Planet(0.95, 0.72, 0.62);
+    Venus.orbit();
+    const Earth = new Planet(1,1,1);
+    Earth.orbit();
     const Mars = new Planet(0.53, 1.52, 1.88);
-
-
-
-
-
-
-
-
-    // ----------軌道
-    createLine();
-
-    //*****************************************
-    // 線を作成
-    //*****************************************
-    function createLine() {
-      const points = [];
-      const pointNum = 360;
-      for (let i = 0; i <= pointNum; i++) {
-        const rad = toRad(360 / pointNum * i);
-
-        const x = earthOrbitRaddius * Math.cos(rad);        
-        const y = 0;
-        const z = earthOrbitRaddius * Math.sin(rad);;
-        const p = new THREE.Vector3(x, y, z);
-        points.push(p);
-      }
-      const geo = new THREE.BufferGeometry().setFromPoints(points);
-      const mat = new THREE.LineBasicMaterial({ color: 0x000000 });
-      const line = new THREE.Line(geo, mat);
-      scene.add(line);
-    }
-
-
-
-
-
-
-
+    Mars.orbit();
+    const Jupiter = new Planet(11.21, 5.2, 11.9);
+    Jupiter.orbit();
+    const Saturn = new Planet(9.45, 9.58, 29.4);
+    Saturn.orbit();
 
     tick();
 
     // 毎フレーム時に実行されるループイベントです
     function tick() {
-      // -----地球
-      earthPhi += earthPhiSpeed;
-      // earthTheta += earthThetaSpeed;
-      const earthX = earthOrbitRaddius * Math.cos(earthPhi);
-      const earthY = earthOrbitRaddius * Math.sin(earthTheta);
-      const earthZ = earthOrbitRaddius * Math.sin(earthPhi);
-      Earth.position.set(earthX, earthY, earthZ);
+      
       // -----水星
       Mercury.update();
       Venus.update();
+      Earth.update();
       Mars.update();
-
+      Jupiter.update();
+      Saturn.update();
       renderer.render(scene, camera); // レンダリング
       requestAnimationFrame(tick);
     }
