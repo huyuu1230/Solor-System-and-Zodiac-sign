9918d785c0040dde1e7f50b3b0081b946aa16b79
nirnorOkazaki <okazaki@nirnor.jp>
2023-07-02 23:23:59 +0900

    orbitControllを追加 クリックイベントを追加中

--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,18 @@
 
 <script setup>
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function toRad(deg) {
   return (deg * Math.PI) / 180;
 }
 
 onMounted(() => {
+  const canvas = document.querySelector("#index");
+  // -----クリック関連
+  let mouse;
+  let raycaster;
+
   let scene, camera, renderer;
   let width = window.innerWidth;
   let height = window.innerHeight;
@@ -28,10 +34,37 @@ onMounted(() => {
     camera = new THREE.PerspectiveCamera(100, width / height, 5000, 0);
     camera.position.set(0, 5000, +20000);
 
+    // -----Orbit_Controls
+    const controls = new OrbitControls(camera, canvas)
+
+    
+    // clickEvent
+    mouse = new THREE.Vector2();
+    raycaster = new THREE.Raycaster();
+    canvas.addEventListener('mousemove', handleMouseMove);
+
+    function handleMouseMove(event) {
+      const element = event.currentTarget;
+      //canvas上のマウスのXY座標
+      const x = event.clientX - element.offsetLeft;
+      const y = event.clientY - element.offsetTop;
+      //canvasの幅と高さを取得
+      const w = element.offsetWidth;
+      const h = element.offsetHeight;
+      //マウス座標を-1〜1の範囲に変換
+      mouse.x = (x / w) * 2 - 1;
+      mouse.y = -(y / h) * 2 + 1;
+      console.log(mouse.x);
+      console.log(mouse.y);
+    }
+
+
+
+
     // -----規準となる地球の変数を定義
-    const earthRaddius = 100;
+    const earthRaddius = 1000;
     // 地球の軌道半径
-    const earthOrbitRaddius = 1200;
+    const earthOrbitRaddius = 12000;
     // 緯度(方位角)の変化する値
     const earthPhiSpeed = toRad(1);
     // 経度(仰角)の変化する値
@@ -41,13 +74,21 @@ onMounted(() => {
 
     // -----1秒に60回実行される
     // -----６秒で360回
-    
+
     // ----------惑星を生成するクラス
     class Planet {
-      constructor(radius, orbitRadius, phiSpeed,rotation) {
+      constructor(radius, orbitRadius, phiSpeed, rotation) {
         this.r = earthRaddius * radius;
-        this.w = this.r / 50;
-        this.h = this.r / 50;
+        if (radius <= 1) {
+          this.w = this.r / 70;
+          this.h = this.r / 70;
+        } else if (radius > 1 && radius <= 5) {
+          this.w = this.r / 500;
+          this.h = this.r / 500;
+        } else {
+          this.w = this.r / 500;
+          this.h = this.r / 500;
+        }
         this.or = earthOrbitRaddius * orbitRadius;
         this.phi = toRad(0);
         this.theta = toRad(0);
@@ -93,7 +134,6 @@ onMounted(() => {
         this.y = this.or * Math.sin(this.theta);
         this.z = this.or * Math.sin(this.phi);
         this.rotationY += this.rotationSpeed;
-
         this.planet.position.set(this.x, this.y, this.z);
         this.planet.rotation.y = this.rotationY;
       }
@@ -105,37 +145,55 @@ onMounted(() => {
     // phiSpeed : 公転速度 ,
     // rotation : 自転速度
     // -----惑星
-    const Mercury = new Planet(0.38, 0.39, 0.24,1);
+    const Mercury = new Planet(0.38, 0.39, 0.24, 1);
     Mercury.orbit();
-    const Venus = new Planet(0.95, 0.72, 0.62,1);
+    const Venus = new Planet(0.95, 0.72, 0.62, 1);
     Venus.orbit();
-    const Earth = new Planet(1,1,1,1);
+    const Earth = new Planet(1, 1, 1, 1);
     Earth.orbit();
-    const Mars = new Planet(0.53, 1.52, 1.88,1);
+    const Mars = new Planet(0.53, 1.52, 1.88, 1);
     Mars.orbit();
-    const Jupiter = new Planet(11.21, 5.2, 11.9,1);
+    const Jupiter = new Planet(11.21, 5.2, 11.9, 1);
     Jupiter.orbit();
-    const Saturn = new Planet(9.45, 9.58, 29.4,1);
+    const Saturn = new Planet(9.45, 9.58, 29.4, 1);
     Saturn.orbit();
-    const Uranus = new Planet(4.01,19.2,83.8,1);
+    const Uranus = new Planet(4.01, 19.2, 83.8, 1);
     Uranus.orbit();
-    const Neptune = new Planet(3.88,30.0,163.8,1);
+    const Neptune = new Planet(3.88, 30.0, 163.8, 1);
     Neptune.orbit();
 
     tick();
 
+    
+
     // 毎フレーム時に実行されるループイベントです
     function tick() {
-      
+      // -----Orbit_Controls
+      controls.update();
       // -----惑星
-      Mercury.update();
-      Venus.update();
-      Earth.update();
-      Mars.update();
-      Jupiter.update();
-      Saturn.update();
-      Uranus.update();
-      Neptune.update();
+      // Mercury.update();
+      // Venus.update();
+      // Earth.update();
+      // Mars.update();
+      // Jupiter.update();
+      // Saturn.update();
+      // Uranus.update();
+      // Neptune.update();
+
+      //マウス位置からまっすぐに伸びる光線ベクトルを生成
+      raycaster.setFromCamera(mouse, camera);
+
+      //光線と交差したオブジェクトを取得
+
+      const intersects = raycaster.intersectObjects(scene.children);
+      // console.log(scene.children);
+      // console.log(intersects);
+
+      if (intersects.length > 0) {
+        console.log('intersects')
+      }
+
+
 
       // -----レンダリング
       renderer.render(scene, camera);
